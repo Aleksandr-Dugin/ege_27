7c26dcb47aacbd1b6c03b4748b63e28b63804980
Aleksandr-Dugin <grishaiv13@gmail.com>
2021-12-07 12:13:47 +0300

    Added function

--- main.cpp
+++ main.cpp
@@ -2,10 +2,16 @@
 
 using namespace std; 
 
+void number_of_products_div(){
+
+	
+	
+
+}
+
 
 int main(){ 
-    
-    cout << "Hello world" << endl; 
+   number_of_products_div(); 
 
     return 0; 
 }
