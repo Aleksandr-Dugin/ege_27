b779eae3b001d7c7208fc2dc59fa226a6f234278
Aleksandr-Dugin <grishaiv13@gmail.com>
2021-12-07 19:34:40 +0000

    added development plan

--- main.cpp
+++ main.cpp
@@ -5,7 +5,18 @@ using namespace std;
 
 void number_of_products_div(){
 
+    /* 
+        1. считать число     
+        2. добавить его к одному из счетчиков соответствующего класса эквивалентности
+        3. ответ
+     
+     **/
+
+
+
 }
+
+
 int main(){ 
     number_of_products_div();
     
