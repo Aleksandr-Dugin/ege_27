433b0746c3f999163d883efc0249e3cc757667cf
Aleksandr-Dugin <grishaiv13@gmail.com>
2021-12-07 16:21:23 +0000

    show divideable numbers

--- main.cpp
+++ main.cpp
@@ -11,14 +11,19 @@ void number_of_products_div(){
      **/
 
     long long int n(0), cur(0);
-
+    int n35(0), n7(0), n5(0), no(0);
 
     cin >> n; 
     for (int i(0); i < n; i++){
         cin >> cur; 
-        cout << cur << endl; 
+        if (cur % 35 == 0) n35++; 
+        else if (cur % 5 == 0) n5++; 
+        else if (cur % 7 == 0) n7++; 
     }
     
+    cout << n35 << endl;
+    cout << n5<< endl;
+    cout << n7 << endl;
 }
 
 
