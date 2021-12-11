58289621a94230a5905c5006b5066eda4d161f32
Aleksandr-Dugin <grishaiv13@gmail.com>
2021-12-11 20:44:07 +0000

    show all pairs

--- main.cpp
+++ main.cpp
@@ -5,23 +5,26 @@ using namespace std;
 
 void number_of_sums_div(){
     /** 
-    1.считать и заполнить все числа 
-    2. вывести все пары чисел 
+    + 1.считать и заполнить все числа и вывести  
+    + 2. вывести все пары чисел 
     3. вывести вместо пар сумму чисел
     4. проверить делимость суммы 
     5. найти и вывести наиобольшую сумму 
      **/
         
-    int n; 
+    int n, tmp; 
     cin >> n; 
     vector<int> numbers(n); 
     for (int i =0; i < n; i++){
         cin >> numbers[i]; 
     }        
-    for (int i =0; i < n; i++){
-        cout << numbers[i] << endl;
-    }        
+     
 
+    for (int i =0; i < n - 1; i++){
+        for (int j = i + 1; j < n; j++){
+            cout << numbers[i] << " " << numbers[j] << endl;     
+        }
+    }
 
 
 } 
