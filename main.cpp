1696022e43d34bfb8563789633dc83da8c308188
Aleksandr-Dugin <grishaiv13@gmail.com>
2021-12-14 14:02:39 +0000

    added development plan

--- main.cpp
+++ main.cpp
@@ -2,7 +2,16 @@
 
 using namespace std; 
 
-void min_sum_min_distance_ineff(){}
+void min_sum_min_distance_ineff(){
+    /** 
+    1. считать и заполнить числа 
+    2. вывести все пары чисел 
+    3. вместо пар вывести сумму 
+    4. вывести минимальную сумму 
+     */
+        
+
+}
 
 
 
