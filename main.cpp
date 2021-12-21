873fab21de1747bdb15f89e353782c6b5d85c825
Aleksandr-Dugin <grishaiv13@gmail.com>
2021-12-21 14:03:52 +0300

    reads and writes numbers

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
 #include <iostream> 
-
+#include <vector> 
 using namespace std; 
 
-void effective_solution(){}
+void effective_solution(){
+	/*
+		1.Ввод и выввод входных данных 
+		2.
+	  **/
+
+
+
+
+	int n;
+	cin >> n;
+	vector<int> numbers(n);
+
+	for (int i = 0; i < n; i++){
+		cin >> numbers[i];
+	}
+
+	for (int i = 0; i < n; i++){
+		cout << numbers[i] << endl;
+	}
+
+
+}
 
 
 int main(){ 
