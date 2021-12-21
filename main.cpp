53cf698ad7eed89c4fd77cbb84ad4c558eea749d
Aleksandr-Dugin <grishaiv13@gmail.com>
2021-12-21 01:50:47 +0000

    reads numbers

--- main.cpp
+++ main.cpp
@@ -1,17 +1,29 @@
 #include <iostream> 
-
+#include <vector> 
 using namespace std; 
 
 
 void num_sum_max_dist(){
     /* 
-    1)считать последовательность чисел
+    1)считать последовательность чисел и вывеcти 
     2) вывести пары удовлетворяющие первому условию
     3) вывести пары удовлетворяющие второму условию
     4) вывести ответ
 
 
     **/
+    int n; 
+    
+    cin >> n;   
+    
+    
+    vector<int> numbers(n); 
+    for (int i = 0; i < n; i++){
+        cin >> numbers[i];
+    }
+
+
+
 
 }
 
